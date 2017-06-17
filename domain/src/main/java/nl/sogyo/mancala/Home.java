2f92a0bc4485fd683318f70beb82cffb519bfe8b
Ivo Limmen <ivo.limmen@qsd.nl>
2017-06-17 12:03:09 +0200

    Fomatted this code.

--- domain/src/main/java/nl/sogyo/mancala/Home.java
+++ domain/src/main/java/nl/sogyo/mancala/Home.java
@@ -1,37 +1,35 @@
 package nl.sogyo.mancala;
 
-
 /**
  * The Home of a player, where he collects all the beads.
- * 
+ *
  * @author Ivo Limmen
  */
 public class Home extends Store {
-    
+
     public Home(Player player, int beads) {
 
-	super(player, beads);
+        super(player, beads);
     }
-        
+
     @Override
     public boolean isHome() {
 
         return true;
     }
-    
+
     @Override
     public Store placeBeads(Player player, Store rivalStore, int beads) {
 
-	// your own home?
-	if (getPlayer().equals(player)) {
+        // your own home?
+        if (getPlayer().equals(player)) {
 
-	    // plce beads as normal
-	    return super.placeBeads(player, rivalStore, beads);
-	}
-	else {
-	 
-	    // skip this store and go to the next one
-	    return nextStore().placeBeads(player, rivalStore, beads);
-	}
+            // plce beads as normal
+            return super.placeBeads(player, rivalStore, beads);
+        } else {
+
+            // skip this store and go to the next one
+            return nextStore().placeBeads(player, rivalStore, beads);
+        }
     }
 }
